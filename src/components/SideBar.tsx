98fe456d145e7e25f61bbb89ba9fe2a4794d13f0
Tárik Wataya <tarikwataya@gmail.com>
2021-03-16 19:38:58 -0300

    ✔ Conclusão do desafio de componentização + ajustes gerais

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Button } from './Button';
+import { GenreResponseProps } from '../util/PropsMoviesGenres';
+import { api } from '../services/api';
+
+interface SideBarProps{
+  handleClickButton:(id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleSelectId(id: number) {
+    props.handleClickButton(id);
+  }
+
+  return (
+    <nav className="sidebar">
+    <span>Play<p>Movies</p></span>
+  
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          id={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleSelectId(genre.id)}
+          selected={props.selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  
+  </nav>
+  )
 }
